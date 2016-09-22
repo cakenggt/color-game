4323fc346ee213811ec2aadf579c35c4589b7afa
Alec Lownes <alec.lownes@gmail.com>
2016-09-22 17:19:33 -0400

    Initial commit of working game

---
+++ app/index.jsx
@@ -0,0 +1,98 @@
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      color: Math.floor(Math.random()*16777216),
+      submittedColor: ''
+    };
+  },
+  render: function() {
+    var contentStyle = {
+      backgroundColor: '#'+this.state.color.toString(16)
+    };
+    return (
+      <div
+        className="content"
+        style={contentStyle}>
+        <div
+          className="hash">
+          <span
+            onClick={this.submitColor}>#</span>
+          <input
+            onChange={this.changeColor}
+            onKeyPress={this.handleKeyPress}
+            type="text"
+            size="6"
+            value={this.state.submittedColor}/>
+        </div>
+        <div
+          id="curve_chart"></div>
+      </div>
+    );
+  },
+  changeColor: function(e){
+    this.setState({submittedColor: e.target.value.toUpperCase()});
+  },
+  handleKeyPress: function(e){
+    if (e.key === 'Enter'){
+      this.submitColor();
+    }
+  },
+  submitColor: function(){
+    var diff = Math.abs(parseInt(this.state.submittedColor, 16) - this.state.color);
+    var scoreHistory = localStorage.scoreHistory;
+    if (!scoreHistory){
+      scoreHistory = []
+    }
+    else{
+      scoreHistory = JSON.parse(scoreHistory);
+    }
+    scoreHistory.push(diff);
+    localStorage.setItem('scoreHistory', JSON.stringify(scoreHistory));
+    this.setState({
+      submittedColor: '',
+      color: Math.floor(Math.random()*16777216)
+    });
+    drawChart();
+  }
+});
+
+render(
+  <App/>,
+  document.getElementById('app'));
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(){
+  var scoreHistory = localStorage.scoreHistory;
+  if (!scoreHistory){
+    scoreHistory = []
+  }
+  else{
+    scoreHistory = JSON.parse(scoreHistory);
+  }
+  console.log(scoreHistory);
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'index');
+  data.addColumn('number', 'score');
+  data.addRows(scoreHistory.map(function(element, i){
+    return [i, element];
+  }));
+
+  var options = {
+    title: 'Scores',
+    curveType: 'function',
+    legend: { position: 'bottom' },
+    vAxis: {
+      scaleType: 'log'
+    }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+};
